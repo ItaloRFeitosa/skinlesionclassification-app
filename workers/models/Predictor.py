6bd06b5c61f55d973c277545b3fb594340c8d62e
ItaloRFeitosa <italo85199@gmail.com>
2020-06-25 14:20:06 -0300

    Add Predictors Layer

---
+++ workers/models/Predictor.py
@@ -0,0 +1,15 @@
+import numpy as np
+from joblib import load
+from pathlib import Path
+
+class Predictor:
+
+    def __init__(self, model_name):
+        # self.model_name = model_name.split("_").join(" ").title()
+        path = str(Path('./models', model_name).with_suffix('.joblib'))
+        self.classifier = load(path)
+
+    def predict(self, feature):
+        pred = np.ravel(self.classifier.predict_proba(np.reshape(feature, (1, -1))))
+        pred = [(round(p,4)*100) for p in pred]
+        return pred
--- workers/utils.py
+++ workers/utils.py
@@ -1,4 +1,6 @@
-def normalizar(x):
+import numpy as np
+
+def normalize(x):
     x_mean = np.mean(x)
     x_std = np.std(x)
     x = (x - x_mean)/x_std
