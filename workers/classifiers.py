8d1795c27d1f1b91d7261874f93df96af602b7e7
ItaloRFeitosa <italo85199@gmail.com>
2020-06-25 14:12:04 -0300

    Changes for testing

--- workers/classifiers.py
+++ workers/classifiers.py
@@ -6,11 +6,9 @@ from features_extraction import vgg16
 from features_extraction import resnet50
 from features_extraction import inceptionv3
 import numpy as np
+from pathlib import Path
 
 CURRENT_DIR = os.getcwd()
-os.chdir('..')
-IMAGE_DIR = os.getcwd() + '\\web-app\\static\\upload\\'
-os.chdir('workers')
 
 def make_dict(pred, name):
         return {
@@ -19,14 +17,9 @@ def make_dict(pred, name):
             'max' : int(np.argmax(pred))
         }
 
-def predict(filename):
-    
-    modelspath = CURRENT_DIR + '\\models'
-
-    imgpath = IMAGE_DIR + filename
-    
-    print(imgpath)
-    predictions = { 'preds': [],
+def init_predictions():
+    return { 
+        'preds': [],
         'labels' : ['akiec','bcc','bkl','df','mel','nv','vasc'],
         'descricao' : ['Actinic Keratoses',
                         'Basal cell carcinoma',
@@ -39,27 +32,67 @@ def predict(filename):
         'media' : list([0, 0, 0, 0, 0, 0, 0]),
         'binary': list([0,0])
     }
+
+def 
+
+def predict(imgpath):
+    modelspath = str(Path('./models'))
+    
+    predictions = init_predictions()
+
     predictions['preds'] = []
 
-    predictions['preds'].append(make_dict(xception.svm(imgpath, modelspath, 'rbf', False), 'Xception + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(xception.svm(imgpath, modelspath, 'rbf', True), 'Xception + normalização + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(xception.svm(imgpath, modelspath, 'linear', False), 'Xception + SVM (Kernel linear)'))
-    predictions['preds'].append(make_dict(xception.svm(imgpath, modelspath, 'linear', True), 'Xception + normalização + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        xception.svm(imgpath, modelspath, 'rbf', False), 
+        'Xception + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        xception.svm(imgpath, modelspath, 'rbf', True), 
+        'Xception + normalização + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        xception.svm(imgpath, modelspath, 'linear', False), 
+        'Xception + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        xception.svm(imgpath, modelspath, 'linear', True), 
+        'Xception + normalização + SVM (Kernel linear)'))
 
-    predictions['preds'].append(make_dict(vgg16.svm(imgpath, modelspath, 'rbf', False), 'VGG16 + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(vgg16.svm(imgpath, modelspath, 'rbf', True), 'VGG16 + normalização + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(vgg16.svm(imgpath, modelspath, 'linear', False), 'VGG16 + SVM (Kernel linear)'))
-    predictions['preds'].append(make_dict(vgg16.svm(imgpath, modelspath, 'linear', True), 'VGG16 + normalização + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        vgg16.svm(imgpath, modelspath, 'rbf', False), 
+        'VGG16 + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        vgg16.svm(imgpath, modelspath, 'rbf', True), 
+        'VGG16 + normalização + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        vgg16.svm(imgpath, modelspath, 'linear', False), 
+        'VGG16 + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        vgg16.svm(imgpath, modelspath, 'linear', True), 
+        'VGG16 + normalização + SVM (Kernel linear)'))
 
-    predictions['preds'].append(make_dict(resnet50.svm(imgpath, modelspath, 'rbf', False), 'ResNet50 + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(resnet50.svm(imgpath, modelspath, 'rbf', True), 'ResNet50 + normalização + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(resnet50.svm(imgpath, modelspath, 'linear', False), 'ResNet50 + SVM (Kernel linear)'))
-    predictions['preds'].append(make_dict(resnet50.svm(imgpath, modelspath, 'linear', True), 'ResNet50 + normalização + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        resnet50.svm(imgpath, modelspath, 'rbf', False), 
+        'ResNet50 + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        resnet50.svm(imgpath, modelspath, 'rbf', True), 
+        'ResNet50 + normalização + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        resnet50.svm(imgpath, modelspath, 'linear', False), 
+        'ResNet50 + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        resnet50.svm(imgpath, modelspath, 'linear', True), 
+        'ResNet50 + normalização + SVM (Kernel linear)'))
 
-    predictions['preds'].append(make_dict(inceptionv3.svm(imgpath, modelspath, 'rbf', False), 'InceptionV3 + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(inceptionv3.svm(imgpath, modelspath, 'rbf', True), 'InceptionV3 + normalização + SVM (Kernel Rbf)'))
-    predictions['preds'].append(make_dict(inceptionv3.svm(imgpath, modelspath, 'linear', False), 'InceptionV3 + SVM (Kernel linear)'))
-    predictions['preds'].append(make_dict(inceptionv3.svm(imgpath, modelspath, 'linear', True), 'InceptionV3 + normalização + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        inceptionv3.svm(imgpath, modelspath, 'rbf', False), 
+        'InceptionV3 + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        inceptionv3.svm(imgpath, modelspath, 'rbf', True), 
+        'InceptionV3 + normalização + SVM (Kernel Rbf)'))
+    predictions['preds'].append( make_dict(
+        inceptionv3.svm(imgpath, modelspath, 'linear', False), 
+        'InceptionV3 + SVM (Kernel linear)'))
+    predictions['preds'].append( make_dict(
+        inceptionv3.svm(imgpath, modelspath, 'linear', True), 
+        'InceptionV3 + normalização + SVM (Kernel linear)'))
 
     for preds in predictions['preds']:
         if ((preds['pred'][0]>10) or (preds['pred'][1]>10) or (preds['pred'][4]>10)):
@@ -78,7 +111,7 @@ def predict(filename):
 def main(arg1):
     predictions = predict(arg1)
     filename = arg1.rsplit('.', 1)[0] + '.json'
-    path = CURRENT_DIR + '\\predictions\\'+filename
+    path = filename
     with open(path, 'w') as json_file:
         json.dump(predictions, json_file)
     
