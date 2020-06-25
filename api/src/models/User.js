c2599accdb5acba9f0886a788b03e7559a754a8a
ItaloRFeitosa <italo85199@gmail.com>
2020-06-25 14:05:54 -0300

    Remove not used files

--- api/src/models/User.js
+++
@@ -1,9 +0,0 @@
-const db = require('../database/db');
-const Model = require('./Model'); 
-const usersCollection = db('users');
-
-const User = Model( usersCollection );
-
-module.exports = {
-    ...User,
-};
