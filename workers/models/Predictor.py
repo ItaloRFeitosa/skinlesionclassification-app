96d600e2ab7c513ccc4ba8ed44cd3358a378ec58
ItaloRFeitosa <italo85199@gmail.com>
2020-06-27 16:34:50 -0300

    Add model_name

--- workers/models/Predictor.py
+++ workers/models/Predictor.py
@@ -5,7 +5,7 @@ from pathlib import Path
 class Predictor:
 
     def __init__(self, model_name):
-        # self.model_name = model_name.split("_").join(" ").title()
+        self.model_name = model_name
         path = str(Path('./models', model_name).with_suffix('.joblib'))
         self.classifier = load(path)
 
