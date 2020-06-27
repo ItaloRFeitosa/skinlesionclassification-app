c20339afb1e2527ee98b9de181adc6968cc26a5f
ItaloRFeitosa <italo85199@gmail.com>
2020-06-27 16:34:37 -0300

    Refactor

---
+++ workers/services/FirebaseService.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+from pathlib import Path
+
+class FirebaseService:
+    def __init__(self):
+        serviceAccount = str(Path("../", "serviceAccountKey.json"))
+
+        cred = credentials.Certificate(serviceAccount)
+        firebase_admin.initialize_app(cred, {
+            'storageBucket': 'skincc-3af45.appspot.com'
+        })
+
+        self.bucket = storage.bucket()
--- workers/worker.py
+++ workers/worker.py
@@ -1,14 +1,7 @@
 from celery import Celery
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import storage
 
-from features_extraction.cnn_config import inceptionv3_config
-from features_extraction.cnn_config import resnet50_config
-from features_extraction.cnn_config import xception_config
-from features_extraction.cnn_config import vgg16_config
-from features_extraction.FeatureExtractor import FeatureExtractor
-from PredictionWithSVM import PredictionWithSVM
+from services.FirebaseService import FirebaseService
+from Predictions import Predictions
 
 from pathlib import Path
 import subprocess
@@ -17,43 +10,24 @@ import os
 import json
 import time
 
+firebase_service = FirebaseService()
 
-serviceAccount = str(Path("..", "serviceAccountKey.json"))
-
-cred = credentials.Certificate(serviceAccount)
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'skincc-3af45.appspot.com'
-})
-
-bucket = storage.bucket()
+predictions = Predictions()
 
 app = Celery('worker', backend='rpc://', broker='amqp://worker_user:worker_user@localhost/service_vhost')
 
 
-inceptionv3_feature_extractor = FeatureExtractor( **inceptionv3_config )
-vgg16_feature_extractor = FeatureExtractor( **vgg16_config )
-resnet50_feature_extractor = FeatureExtractor( **resnet50_config )
-xception_feature_extractor = FeatureExtractor( **xception_config )
-
 def download_image(img_name):
-    blob = bucket.blob(img_name)
+    blob = firebase_service.bucket.blob(img_name)
     donwloaded_image = str(Path("./downloads", img_name))
     blob.download_to_filename(donwloaded_image)
     return donwloaded_image
-
-def run_prediction(img_path, features_extractor):
-    prediction_with_svm = PredictionWithSVM(features_extractor)
-    result = prediction_with_svm.start(img_path)
-    return result
 
 @app.task(bind=True, name='predict', serializer='json')
 def predict(self, taskData):
 
     image_to_predict = download_image(taskData['img_name'])
 
-    result1 = run_prediction(image_to_predict, inceptionv3_feature_extractor)
-    result2 = run_prediction(image_to_predict, vgg16_feature_extractor)
-    result3 = run_prediction(image_to_predict, resnet50_feature_extractor)
-    result4 = run_prediction(image_to_predict, xception_feature_extractor)
+    results = predictions.run(image_to_predict)
     
-    return taskData
+    return results
